aaf9abbc7bd21ae9d5f33ee0d54c10e48f5dd896
LuXRocK <samuelujazda@gmail.com>
2024-02-12 13:27:34 +0100

    solved this problem with a struggle, it tooke me over an hour but finally i managed to do it, I think it was a very engaging problem

---
+++ assign_cookies/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+int findContentChildren(std::vector<int>& g, std::vector<int>& s){
+    int result = 0;
+    sort(g.begin(), g.end());
+    sort(s.begin(), s.end());
+
+    for(int i = 0; i < g.size()-1; i++){
+        int temp = result;
+        for(int j = 0; j < s.size(); j++){
+            if(s[j] == g[i]){
+                result ++;
+                s.erase(s.begin() + j);
+                break;
+            }
+        }
+        if(result == temp){
+            for(int j = 0; j < s.size(); j++){
+                if(s[j] > g[i]){
+                    result ++;
+                    s.erase(s.begin() + j);
+                    break;
+                }
+            }
+        }
+    }
+    return result;
+
+}
+
+int main(){
+    std::vector<int> g{1,2,3};
+    std::vector<int> s{1,1};
+
+    sort(s.begin(), s.end());
+
+   
+    std::cout << findContentChildren(g, s) << std::endl;
+    return 0;
+}
