0a0f9952fc964747a823303109549a10489464b7
LuXRocK <samuelujazda@gmail.com>
2024-02-13 13:26:24 +0100

    this solution with less loops worked, it doesnt go through every possible substring of the string and thats why its faster

--- repeated_substring_pattern/main.cpp
+++ repeated_substring_pattern/main.cpp
@@ -4,25 +4,22 @@
 bool repeatedSubstringPattern(std::string s){
     std::string ns;
     for(int i = 0; i < s.length(); i++){
-        for (int j = 1; j <= s.length()-1; j++){
-            ns = "";
-            std::string subs = s.substr(i, j);
-            while(ns.length() < s.length()){
-                ns += subs;
-                if(ns == s){
-                    return true;
-                }
+        ns = "";
+        std::string subs = s.substr(0, i);
+        while(ns.length() < s.length() && subs.length() != 0){
+            ns += subs;
+            if(ns == s){
+                return true;
             }
-        }
+        } 
     }
     return false;
-    
 }
 
 int main(){
 
-    std::string s = "abcabcabcabc";
+    std::string s = "aba";
     std::cout << repeatedSubstringPattern(s) << std::endl;
-    
+
     return 0;
 }
