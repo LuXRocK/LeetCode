3fa3fe2ba688fea5d1172d2806c896e56fe133a3
LuXRocK <samuelujazda@gmail.com>
2024-02-15 20:50:46 +0100

    added description of the problem and soleved it, it was pretty easy once I knew how to convert decimal to binary which i learned a few problems ago

--- number_complement/main.cpp
+++ number_complement/main.cpp
@@ -3,6 +3,7 @@
 #include <bits/stdc++.h>
 
 int findComplement(int num){
+    int result = 0;
     std::vector<int> binNum;
     while(num > 0){
         if(num%2 == 1){
@@ -12,20 +13,24 @@ int findComplement(int num){
         }
         num /= 2;
     }
+    int exp = 0;
     for(auto i : binNum){
         if(i == 1){
             i = 0;
+            result += i*pow(2, exp);
         }else{
             i = 1;
+            result += i*pow(2, exp);
         }
+        exp++;
     }
-
+    return result;
 }
 
 
 
 int main(){
 
-    findComplement(5);
+    findComplement(1);
     return 0;
 }
